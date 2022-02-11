dde1636fea5bfa1ee3bf92f790abb291acc01d56
Danylo Donets <EndoBroYT@gmail.com>
2022-02-11 16:26:35 +0200

    Update Exercise.java

--- src/Exercise.java
+++ src/Exercise.java
@@ -5,16 +5,16 @@ public class Exercise
 {
     public static boolean Amstrong_Number(int number)
     {
-        int[] numbers;
+        int[] numbers;//Створюю масив
         numbers = new int[(int) (Math.log10(number) + 1)];
-        int check_number = 0;
-        int step = (int) (Math.log10(number) + 1);
-        int buff = number;
+        int check_number = 0; // Змінна суми чисел
+        int step = (int) (Math.log10(number) + 1); //Кількість цифр в числах
+        int buff = number;//Буфер
         for(int i = 0; i < step; i++)
         {
-            numbers[i] = buff%10;
-            check_number += Math.pow(numbers[i],step);
-            buff /= 10;
+            numbers[i] = buff%10;//Узнаю останнє число 
+            check_number += Math.pow(numbers[i],step);//підношу число до степеня та сумую його в змінну check_number
+            buff /= 10;// віднімаю число яке підносив в зміну check_number
         }
         if(check_number == number)
         {
