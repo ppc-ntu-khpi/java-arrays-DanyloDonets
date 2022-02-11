4153c36bc4a51dcf70cfee1be4c553194ba8f9e3
Danylo Donets <EndoBroYT@gmail.com>
2022-02-11 16:20:02 +0200

    Update TestResult.java

--- src/TestResult.java
+++ src/TestResult.java
@@ -8,11 +8,11 @@ public class TestResult
 
     public static void main(String[] args) 
     {
-        Scanner console = new Scanner(System.in); 
+        Scanner console = new Scanner(System.in); //Викликаємо сканер
         System.out.println("Write number:");
-        int number = console.nextInt();
-        console.close();
+        int number = console.nextInt();//Скануємо число
+        console.close();// ВИключаємо сканер
         
-        System.out.println("Is your number Amstrong`s number:"+Exercise.Amstrong_Number(number));
+        System.out.println("Is your number Amstrong`s number:"+Exercise.Amstrong_Number(number));//Вивожу результат з функції Amstrong_Number
     }
 }
